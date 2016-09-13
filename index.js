54a8546cc066c17d5e21571835bc8333f6b6e2b9
Joah Gerstenberg <me@joahg.com>
2016-09-13 16:33:48 -0500

    set up express to satisfy heroku

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send('Everything is 200 OK');
+});
+
+app.listen(process.env.PORT || 8080);
+
+
+
+
 var SlackClient = require('@slack/client');
 
 
