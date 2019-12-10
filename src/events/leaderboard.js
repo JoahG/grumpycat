68f7a3f060eb1dd5d1cfd4e80d643b01126be5d8
Joah Gerstenberg <me@joahg.com>
2019-12-10 17:54:51 -0600

    leaderboard

---
+++ src/events/leaderboard.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import { connection } from '../slack.js';
+import { User } from '../models';;
+
+var LeaderboardHandler = function(message) {
+  User.find({ }, function (err, users) {
+    connection.sendMessage(users.sort(function(a, b) {
+      if (a.karma < b.karma) return 1;
+      if (b.karma < a.karma) return -1;
+      return 0;
+    }).map(function(user) {
+      return `<@${ user.id }>: ${ user.karma }`
+    }).join(`\n`), message.channel);
+  });
+};
+
+export default {
+  exec: LeaderboardHandler,
+  test: function(messageText) {
+    return /^\!decide$/.test(messageText);
+  }
+};
